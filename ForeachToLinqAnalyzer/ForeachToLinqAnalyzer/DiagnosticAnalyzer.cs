87c3d27ac52cd8a5c8cf6b765a9860d6fbb87b51
Nyctef <nyctef@nyctef.com>
2015-06-26 16:46:04 +0100

    remove 'Analyzer' from diagnostic ID

--- ForeachToLinqAnalyzer/ForeachToLinqAnalyzer/DiagnosticAnalyzer.cs
+++ ForeachToLinqAnalyzer/ForeachToLinqAnalyzer/DiagnosticAnalyzer.cs
@@ -14,7 +14,7 @@ namespace ForeachToLinqAnalyzer
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class ForeachToLinqAnalyzer : DiagnosticAnalyzer
     {
-        public const string DiagnosticId = "ForeachToLinqAnalyzer";
+        public const string DiagnosticId = "ForeachToLinq";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         internal static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
